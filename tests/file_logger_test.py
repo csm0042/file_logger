3b5f8be739b37ab6edbe742a660ca77c7294ac55
csmaue@gmail.com <thpbth5150>
2017-01-05 11:25:51 -0600

    Fixed bogus import statement

--- tests/file_logger_test.py
+++ tests/file_logger_test.py
@@ -8,7 +8,7 @@ import unittest
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-from file_logger.file_logger import setup_log_files, setup_log_handlers, setup_logging
+import file_logger
 
 
 # Define test class *******************************************************************************
@@ -18,19 +18,19 @@ class TestFileLogger(unittest.TestCase):
 
 
     def test_setup_log_files(self):
-        self.debug_file, self.info_file = setup_log_files(__file__)
+        self.debug_file, self.info_file = file_logger.setup_log_files(__file__)
         self.assertEqual(self.debug_file, "c:/python_logs/file_logger_test_debug.log")
         self.assertEqual(self.info_file, "c:/python_logs/file_logger_test_info.log")
 
 
     def test_setup_log_handlers(self):
-        self.debug_file, self.info_file = setup_log_files(__file__)
-        self.logger = setup_log_handlers(__file__, self.debug_file, self.info_file)
+        self.debug_file, self.info_file = file_logger.setup_log_files(__file__)
+        self.logger = file_logger.setup_log_handlers(__file__, self.debug_file, self.info_file)
         self.assertEqual(len(self.logger.handlers), 3)
 
 
     def test_setup_logging(self):
-        self.logger = setup_logging(__file__)
+        self.logger = file_logger.setup_logging(__file__)
         self.assertEqual(len(self.logger.handlers), 3)
 
 
